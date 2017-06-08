e3cf094d8df7fb7eb2c499eb47a627ab9298543a
602774014@qq.com <602774041@qq.com>
2017-06-08 10:38:05 +0800

    安卓返回

--- app/index.js
+++ app/index.js
@@ -1,14 +1,14 @@
 /**
  * Created by Rabbit on 2017/4/19.
  */
-import { AppRegistry,View,Text } from 'react-native';
+import { AppRegistry,View,Text,BackHandler } from 'react-native';
 import React, { Component } from 'react';
 import {Provider}from 'react-redux';
 
 import configureStore from './store/ConfigureStore';
 
 const store = configureStore();
-
+let lastClickTime = 0;
 import App from './APP';
 if (!__DEV__) {
     global.console = {
@@ -31,6 +31,31 @@ export default class Root extends Component {
             isLogin:false
         }
     }
+
+
+
+    componentDidMount() {
+        if (Android){
+            BackHandler.addEventListener('handwareBackPress',this.onBackAndroid)
+        }
+    }
+    componentWillUnmount() {
+       if (Android){
+            BackHandler.addEventListener('handwareBackPress',this.onBackAndroid)
+        }
+    }
+
+
+    onBackAndroid=()=>{
+        let now = new Date().getTime();
+        if (now - lastClickTime < 2500 ){
+            return false;
+        }
+        lastClickTime = now ;
+        alert('再按一次退出应用');
+        return true;
+    }
+
     render() {
         return (
             !this.state.isLogin ?
