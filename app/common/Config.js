51534f4d5dc5b64b9bca33002266ff3700f64506
Rabbit <515236489@qq.com>
2017-06-30 14:19:02 +0800

    1.更新二维码

--- app/common/Config.js
+++ app/common/Config.js
@@ -4,13 +4,13 @@
 'use strict';
 
 const base = {
-    baseURL : iOS?'http://localhost:3000/api':'http://172.16.17.11:3000/api',
+    // baseURL : iOS?'http://localhost:3000/api':'http://172.16.17.11:3000/api',
 
 
     // baseURL:iOS?'http://localhost:1234/api':'http://10.0.2.2:1234/api',
 
     // baseURL:!iOS?'http://localhost:1234/api':'http://172.16.17.61:1234/api',
-    // baseURL :'http://shitu.leanapp.cn/api'
+    baseURL :'http://shitu.leanapp.cn/api'
 };
 
 const Config = {
