61b8c2769f28084b268d6f8875193867157c8715
Rabbit <515236489@qq.com>
2018-07-11 14:29:51 +0800

    1. 提交适配代码

---
+++ app/utils/Tool.js
@@ -0,0 +1,17 @@
+/**
+ * @flow
+ * Created by Rabbit on 2018/7/11.
+ */
+
+import {Dimensions, Platform, PixelRatio} from 'react-native';
+const {width} = Dimensions.get('window');
+
+const basePx = Platform.OS === 'ios' ? 750 : 720;
+
+export const px2dp = function px2dp(px: number): number {
+
+	const layoutSize = px / basePx * width;
+
+	return PixelRatio.roundToNearestPixel(layoutSize);
+
+};
