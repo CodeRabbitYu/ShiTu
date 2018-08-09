cdbdce22a252f0a64b4f7195cedc8f290382ba2b
Rabbit <515236489@qq.com>
2018-08-09 23:46:04 +0800

    1. 对福利页图片的加载方式进行修改，从跳转改为弹出式，还需优化

--- app/mobx/News/WelfareMobx.js
+++ app/mobx/News/WelfareMobx.js
@@ -7,6 +7,8 @@ import {observable, computed, action, runInAction, autorun} from 'mobx';
 import type {RTGankResult, RTWeal} from '../../servers/News/interfaces';
 import {loadWelfareData} from '../../servers/News';
 import {System} from '../../utils';
+import FetchBlob from "rn-fetch-blob";
+import {CameraRoll} from "react-native";
 // import {RootStore} from '../../store/RootStore';
 
 let loadMoreNumber = [];
@@ -16,6 +18,51 @@ class WelfareMobx {
   @observable isRefreshing: boolean = true;
   @observable dataSource: Array<RTGankResult> = [];
   @observable page: number = 1;
+
+  @observable isModalVisible: boolean = false;
+  @observable imageUrl: string = '';
+  @observable imageLargeUrl: string = '';
+
+  setModalViewData = (isModalVisible, imageUrl, imageLargeUrl) => {
+  	runInAction(() => {
+		  this.isModalVisible = isModalVisible;
+		  this.imageUrl = imageUrl;
+		  this.imageLargeUrl = imageLargeUrl;
+	  });
+  }
+
+	@action.bound
+	saveImageWithIOS = async (url: string) => {
+		try {
+			await  CameraRoll.saveToCameraRoll(url, 'photo');
+			alert('保存成功');
+		} catch (e) {
+			alert('保存失败');
+		}
+	}
+
+	@action.bound
+	saveImageWithAndroid = async (url: string) => {
+		// url最后一个反斜线的位置
+		const lastIndex = url.lastIndexOf('/');
+		// 通过位置得到图片名称
+		const imageName = url.substr(lastIndex);
+
+		const config = {
+			fileCache: true,
+			path: Dirs.DCIMDir + imageName
+		};
+
+		try {
+			// 下载图片
+			await FetchBlob.config(config).fetch('GET', url);
+			alert('保存成功');
+		} catch (e) {
+			console.log(e);
+			alert('保存失败');
+		}
+	}
+
 
   @action.bound
   async fetchWelfareData(page) {
--- app/route/News/Welfare/index.js
+++ app/route/News/Welfare/index.js
@@ -10,7 +10,7 @@ import {
 	View,
 	Image,
 	ActivityIndicator,
-	Modal
+	Modal, TouchableWithoutFeedback
 } from 'react-native';
 import { MasonryList, BaseContainer } from '../../../components';
 
@@ -20,11 +20,17 @@ import { observer} from 'mobx-react';
 import { Button, CustomImage } from '../../../components';
 import type {NavigationState} from 'react-navigation';
 import {RTWeal} from '../../../servers/News/interfaces';
-import FastImage from 'react-native-fast-image';
+import {System} from '../../../utils';
+import {ActionSheet, Overlay} from 'teaset';
+import {inject} from 'mobx-react';
+import {PowerStore} from '../../../store/PowerStore';
 
 type Props = {
 	navigate: NavigationState;
+	powerStore: PowerStore;
 };
+
+@inject('powerStore')
 @observer
 export class Welfare extends React.Component<Props> {
 
@@ -39,12 +45,72 @@ export class Welfare extends React.Component<Props> {
 		await this.welfareMobx.fetchWelfareData(1);
 	}
 
+	setModalViewData = (isModalVisible, url, largeUrl) => {
+		this.welfareMobx.setModalViewData(isModalVisible, url, largeUrl);
+	}
+
+	actionSheetToSaveImage = () => {
+		const { imageUrl } = this.welfareMobx;
+
+		const items = [
+			{
+				title: '保存图片', onPress: () => System.iOS
+					?
+					this.welfareMobx.saveImageWithIOS(imageUrl)
+					:
+					this.welfareMobx.saveImageWithAndroid(imageUrl)
+			},
+			{
+				title: '设置主屏幕',
+				type: 'default',
+				onPress: async () => {
+					// alert('设置成功');
+					await this.props.powerStore.setShiTuBackgroundImage(imageUrl);
+				}
+			},
+		];
+		const cancelItem = {title: '取消'};
+		ActionSheet.show(items, cancelItem);
+	}
+
+
+	showPopCustom(imageSource, fromView) {
+
+
+		console.log('sssssssssss', fromView);
+
+		fromView.measure((x, y, width, height, pageX, pageY) => {
+			const overlayView = (
+				<Overlay.PopView
+					style={{alignItems: 'center', justifyContent: 'center'}}
+					overlayOpacity={1}
+					type='custom'
+					animated={false}
+					customBounds={{x: pageX, y: pageY, width, height}}
+					ref={v => this.customPopView = v}
+				>
+					<TouchableWithoutFeedback
+						onLongPress={this.actionSheetToSaveImage}
+						onPress={() => this.customPopView && this.customPopView.close()}>
+						<CustomImage source={{uri: imageSource}} resizeMode='cover' style={{backgroundColor: 'red', width: SCREEN_WIDTH, height: SCREEN_HEIGHT
+						}}/>
+					</TouchableWithoutFeedback>
+				</Overlay.PopView>
+			);
+			Overlay.show(overlayView);
+		});
+	}
+
 	renderItem = ({item}: {item: RTWeal, index: number, column: number}) => {
 		return (
 			<Button
-				onPress={() => this.props.navigate('WelfareDetail', {url: item.url, largeUrl: item.largeUrl})}
+				// onPress={() => this.props.navigate('WelfareDetail', {url: item.url, largeUrl: item.largeUrl})}
+
+				onPress={() => this.showPopCustom(item.url, this.itemImage)}
+				// onPress={() => this.setModalViewData(true, item.url, item.largeUrl)}
 			>
-				<CustomImage source={{uri: item.url}}
+				<Image source={{uri: item.url}}
+				       ref={v => this.itemImage = v}
 				             style={[
 					             styles.cell,
 					             { height: item.height, backgroundColor: 'white'},
@@ -58,10 +124,9 @@ export class Welfare extends React.Component<Props> {
 		const { isRefreshing } = this.welfareMobx;
 		return (
 			!isRefreshing &&
-				<View style={{height: 50, flex: 1,  alignItems: 'center', justifyContent: 'center'}}>
-					<ActivityIndicator size={'small'}/>
-				</View>
-
+			<View style={{height: 50, flex: 1,  alignItems: 'center', justifyContent: 'center'}}>
+				<ActivityIndicator size={'small'}/>
+			</View>
 		);
 	}
 
@@ -91,7 +156,23 @@ export class Welfare extends React.Component<Props> {
 					onEndReached={loadMoreData}
 				/>
 			</BaseContainer>,
+			<Modal key={'Modal'}
+			       animationType={'fade'}
+			       visible={this.welfareMobx.isModalVisible}
+			>
 
+				<Button onLongPress={this.actionSheetToSaveImage}
+				        onPress={() => this.setModalViewData(false, '', '')}
+				        style={{backgroundColor: 'white', flex: 1}}
+				        activeOpacity={0.9}
+				>
+					<CustomImage style={styles.container}
+					             source={{uri: this.welfareMobx.imageLargeUrl}}
+					             resizeMode={'cover'}
+					/>
+				</Button>
+
+			</Modal>
 		];
 	}
 }
