f1e42fa611eddd8dcd40c60cb0e770d9a05261d0
Rabbit <515236489@qq.com>
2018-09-10 17:37:17 +0800

    1. 修复在安卓4.x系统上闪退的bug

--- android/app/src/main/java/com/shitu/MainApplication.java
+++ android/app/src/main/java/com/shitu/MainApplication.java
@@ -1,15 +1,19 @@
 package com.shitu;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
 import com.facebook.react.ReactApplication;
 import com.getui.reactnativegetui.GetuiPackage;
-import com.imagepicker.ImagePickerPackage;
+//import com.imagepicker.ImagePickerPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 
 
 import com.dylanvann.fastimage.FastImageViewPackage;
+import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -61,4 +65,11 @@ public class MainApplication extends Application implements ReactApplication {
     SoLoader.init(this, /* native exopackage */ false);
     CrashReport.initCrashReport(getApplicationContext(), "f7e825bdb6", false);
   }
+
+  @Override
+  protected void attachBaseContext(Context base) {
+    super.attachBaseContext(base);
+    MultiDex.install(getBaseContext());
+  }
+
 }
