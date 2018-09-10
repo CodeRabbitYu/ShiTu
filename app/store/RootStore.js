38205417284ddad3e423dfcf55f1800554ed4105
Rabbit <515236489@qq.com>
2018-09-10 15:37:58 +0800

    1. 添加安卓多渠道打包 2. 添加largeHeap属性，希望可以解决oom问题

--- app/store/RootStore.js
+++ app/store/RootStore.js
@@ -12,3 +12,36 @@ const powerStore = new  PowerStore();
 
 
 export { configStore, powerStore };
+
+/*
+*
+*
+
+rm -rf /Users/Rabbit/.jenkins/workspace/识兔/android/app/build
+cd /Users/Rabbit/.jenkins/workspace/识兔
+
+
+cd android && ./gradlew assembleRelease
+
+rm -rf ${WORKSPACE}/android/app/build
+
+
+cd ${WORKSPACE}
+
+
+rm -rf ${WORKSPACE}/node_modules
+
+/usr/local/bin/npm install
+
+/usr/local/bin/npm run build
+
+
+mkdir -p ${WORKSPACE}/android/app/src/main/assets/
+
+/usr/local/bin/node ${WORKSPACE}/node_modules/react-native/local-cli/cli.js bundle --platform android --dev false --entry-file ${WORKSPACE}/index.js --bundle-output ${WORKSPACE}/android/app/src/main/assets/index.android.bundle  --sourcemap-output ${WORKSPACE}/android/index.android.bundle.map --assets-dest ${WORKSPACE}/android/app/src/main/res/
+
+rm -rf /Users/Rabbit/.jenkins/workspace/识兔/node_modules
+
+/usr/local/bin/npm install
+
+* */
