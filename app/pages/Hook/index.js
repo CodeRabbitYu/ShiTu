08935bc0f20501439a40934f723b618df3c0ee90
Rabbit <515236489@qq.com>
2019-03-14 23:17:47 +0800

    1. 更新flow版本 2. 去除多余的警告 3. 添加安卓缺少的权限 4. 优化安卓端不得姐列表高度不正确的问题 5. 优化之前的类型代码

---
+++ app/pages/Hook/index.js
@@ -0,0 +1,24 @@
+/**
+ * @flow
+ * Created by Rabbit on 2019-03-14.
+ */
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+type Props = {};
+type State = {};
+export default class index extends React.Component<Props, State> {
+  render() {
+    return (
+      <View style={styles.container}>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
